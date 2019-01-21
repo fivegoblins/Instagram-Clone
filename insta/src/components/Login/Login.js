36903d53f53d9e1d1fc19fd77dd8ff1a7d7169bb
Alexandra <alexswartz9753@gmail.com>
2019-01-21 16:03:51 -0500

    changed = to : in state in Login.js

--- insta/src/components/Login/Login.js
+++ insta/src/components/Login/Login.js
@@ -8,8 +8,8 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username = '',
-            password = ''
+            username: '',
+            password: ''
         };
     }
 
